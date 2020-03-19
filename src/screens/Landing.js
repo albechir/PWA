724b007d313ece6c151ecf3edca7a1a69b68b563
YohannQMR <yohann.quemener@ynov.com>
2020-03-19 15:58:06 +0100

    Save

--- src/screens/Landing.js
+++ src/screens/Landing.js
@@ -1,9 +1,20 @@
 import React from 'react'
+import styled from 'styled-components'
 
 export default function Landing() {
   return (
     <>
-      < />
+      <div>
+          <Virus src="https://www.stickpng.com/assets/images/5bd08b637aaafa0575d85039.png" alt="virus"/>
+          <div>
+              <button>Tentez votre chance au quizz</button>
+          </div>
+      </div>
     </>
   )
 }
+
+const Virus = styled.img`
+  width: 60px;
+  height: 60px;
+`
